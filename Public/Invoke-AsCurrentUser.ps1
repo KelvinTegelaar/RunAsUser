0c7c83a4194023dda0048b5c7616846beff56952
KelvinTegelaar <49186168+KelvinTegelaar@users.noreply.github.com>
2021-03-05 16:11:36 +0100

    updated to version 2.0. Fixed #13 by allowing users to cache the file to disk.

--- Public/Invoke-AsCurrentUser.ps1
+++ Public/Invoke-AsCurrentUser.ps1
@@ -11,13 +11,29 @@ function Invoke-AsCurrentUser {
         [Parameter(Mandatory = $false)]
         [switch]$NonElevatedSession,
         [Parameter(Mandatory = $false)]
-        [switch]$Visible
+        [switch]$Visible,
+        [Parameter(Mandatory = $false)]
+        [switch]$CacheToDisk
     )
     if (!("RunAsUser.ProcessExtensions" -as [type])) {
         Add-Type -TypeDefinition $script:source -Language CSharp
     }
-    $ExpandedScriptBlock = $ExecutionContext.InvokeCommand.ExpandString($ScriptBlock)
-    $encodedcommand = [Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes($ExpandedScriptBlock))
+    if ($CacheToDisk) {
+        $ScriptGuid = new-guid
+        $null = New-item "$($ENV:TEMP)\$($ScriptGuid).ps1" -Value $ScriptBlock -Force
+        $pwshcommand = "-ExecutionPolicy Bypass -Window Normal -file `"$($ENV:TEMP)\$($ScriptGuid).ps1`""
+    }
+    else {
+        $ExpandedScriptBlock = $ExecutionContext.InvokeCommand.ExpandString($ScriptBlock)
+        $encodedcommand = [Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes($ExpandedScriptBlock))
+        $pwshcommand = "-ExecutionPolicy Bypass -Window Normal -EncodedCommand $($encodedcommand)"
+    }
+    $OSLevel = (Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion").CurrentVersion
+    if ($OSLevel -lt 6.2) { $MaxLength = 8190 } else { $MaxLength = 32767 }
+    if ($encodedcommand.length -gt $MaxLength -and $CacheToDisk -eq $false) {
+        Write-Error -Message "The encoded script is longer than the command line parameter limit. Please execute the script with the -CacheToDisk option."
+        return
+    }
     $privs = whoami /priv /fo csv | ConvertFrom-Csv | Where-Object { $_.'Privilege Name' -eq 'SeDelegateSessionUserImpersonatePrivilege' }
     if ($privs.State -eq "Disabled") {
         Write-Error -Message "Not running with correct privilege. You must run this script as system or have the SeDelegateSessionUserImpersonatePrivilege token."
@@ -31,8 +47,9 @@ function Invoke-AsCurrentUser {
             if ($NoWait) { $ProcWaitTime = 1 } else { $ProcWaitTime = -1 }
             if ($NonElevatedSession) { $RunAsAdmin = $false } else { $RunAsAdmin = $true }
             [RunAsUser.ProcessExtensions]::StartProcessAsCurrentUser(
-                $pwshPath, "`"$pwshPath`" -ExecutionPolicy Bypass -Window Normal -EncodedCommand $($encodedcommand)",
+                $pwshPath, "`"$pwshPath`" $pwshcommand",
                 (Split-Path $pwshPath -Parent), $Visible, $ProcWaitTime, $RunAsAdmin)
+            if ($CacheToDisk) { $null = remove-item "$($ENV:TEMP)\$($ScriptGuid).ps1" -Force }
         }
         catch {
             Write-Error -Message "Could not execute as currently logged on user: $($_.Exception.Message)" -Exception $_.Exception
